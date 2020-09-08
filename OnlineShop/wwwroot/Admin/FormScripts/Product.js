fbfd0ebe658c0f27d0ce39cfc10922889fc9ebaa
fahim1384 <fa.azari.a@gmail.com>
2020-09-08 14:26:35 +0430

    990618-2:30

--- OnlineShop/wwwroot/Admin/FormScripts/Product.js
+++ OnlineShop/wwwroot/Admin/FormScripts/Product.js
@@ -34,17 +34,13 @@ function GetAllProduct() {
 
                 Html += `<tr>
                             <td>${i + 1}</td>                            
-                            <td>`+ $(this).attr('OnvaneBlog') + `</td>
-                            <td>`+ item.onvaneBlog + `</td>
-                            <td>${item.languageID}</td>
-                            <td>${item.matneBlog}</td>
-                            <td>${item.linkeBlog}</td>
-                            <td>${item.pinStatus}</td>
-                            <td>${item.emkaneNazardehi}</td>
-                            <td>${item.PinStatus}</td>
-                            <td>${item.tarikheSabt}</td>
-                            <td class="tdTextCenter"><span class="Edit" BlogID=${item.blogID} ><i class="fa fa-edit text text-info"></i></span></td>
-                            <td class="tdTextCenter"><span class="Trash" BlogID=${item.blogID} ><i class="fa fa-trash text text-danger"></i></span></td>
+                            <td>`+ $(this).attr('name') + `</td>
+                            <td>`+ item.price + `</td>
+                            <td>${item.firstCount}</td>
+                            <td>${item.count}</td>
+                            <td>${item.coverImageUrl}</td>  
+                            <td class="tdTextCenter"><span class="Edit" BlogID=${item.id} ><i class="fa fa-edit text text-info"></i></span></td>
+                            <td class="tdTextCenter"><span class="Trash" BlogID=${item.id} ><i class="fa fa-trash text text-danger"></i></span></td>
                        </tr>`;
             });
             Html += `</tbody></table>`;
@@ -59,147 +55,139 @@ function GetAllProduct() {
 
 
 
-//function AddProduct() {
+function AddProduct() {
 
-//    alert(parseInt($('#cmbCategory').val()))
-//    $('#txtOnvaneProduct').val('');
-//    $('#txtEnProduct').val('');
-//    $('#txtCodeProduct').val('');
-//    $('#txtPriceProduct').val('');
-//    $('#MojodiProduct').val('');
-//    $('#elm1').val('');
-//    $('#txtLinkAparat').val('');
+    alert(parseInt($('#cmbCategory').val()))
+    $('#txtOnvaneProduct').val('');
+    $('#txtEnProduct').val('');
+    $('#txtCodeProduct').val('');
+    $('#txtPriceProduct').val('');
+    $('#MojodiProduct').val('');
+    $('#elm1').val('');
+    $('#txtLinkAparat').val('');
 
 
-//    let Product = {
+    let Product = {
  
-//        Name: $('#txtOnvaneProduct').val(),
-//        EnName: $('#txtEnProduct').val(),
-//        CatProductId: 1,
-//        Coding: $('#txtCodeProduct').val(),
-//        Price: $('#txtPriceProduct').val(),
-//        FirstCount: $('#MojodiProduct').val(),
-//        ProductImages: '',
-//        ProductMeterId: parseInt($('#cmbVahed').val()),
-//        MatneProduct: tinyMCE.activeEditor.getContent()
-//    }
+        Name: $('#txtOnvaneProduct').val(),
+        EnName: $('#txtEnProduct').val(),
+        CatProductId: 1,
+        Coding: $('#txtCodeProduct').val(),
+        Price: $('#txtPriceProduct').val(),
+        FirstCount: $('#MojodiProduct').val(),
+        ProductImages: '',
+        ProductMeterId: parseInt($('#cmbVahed').val()),
+        MatneProduct: tinyMCE.activeEditor.getContent()
+    }
 
-//    alert("2")
+    alert("2")
 
-//    jQuery.ajax({
-//        type: "Post",
-//        url: "/api/Product/InsertProduct",
-//        data: JSON.stringify(Product),
-//        async: false,
-//        contentType: "application/json; charset=utf-8",
-//        dataType: "json",
-//        success: function (response) {
-//            //EndLoader();
-//            alert("3")
-//            GetAllProduct();
-//            $('#InsertModal').hide();
-//            $('#PnlList').show();
-//            Swal.fire(
-//                'ثبت شد !',
-//                'درباره ما با موفقیت ثبت شد',
-//                'success'
-//            );
-//        },
-//        error: function (response) {
-//            alert(response)
-//            console.log(response);
-//        },
-//        complete: function () {
-//        }
-//    });
-//}
-//function GetProductById() {
-//    alert("2");
-//    jQuery.ajax({
-//        type: "Get",
-//        url: `/api/Product/GetProductById?id=${Id}`,
-//        data: "",
-//        async: false,
-//        contentType: "application/json; charset=utf-8",
-//        dataType: "json",
-//        success: function (response) {
-
-
-
-//            $('#txtTitleProduct').val("gbcv");
+    jQuery.ajax({
+        type: "Post",
+        url: "/api/Product/InsertProduct",
+        data: JSON.stringify(Product),
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            //EndLoader();
+            alert("3")
+            GetAllProduct();
+            $('#InsertModal').hide();
+            $('#PnlList').show();
+            Swal.fire(
+                'ثبت شد !',
+                'درباره ما با موفقیت ثبت شد',
+                'success'
+            );
+        },
+        error: function (response) {
+            alert(response)
+            console.log(response);
+        },
+        complete: function () {
+        }
+    });
+}
+function GetProductById() {
+    alert("2");
+    jQuery.ajax({
+        type: "Get",
+        url: `/api/Product/GetProductById?id=${Id}`,
+        data: "",
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
 
 
 
-//            $('#InsertModal').show();
-//            $('#PnlList').hide();
-
-//        },
-//        error: function (response) {
-//            alert("13");
-//            console.log(response);
-
-//        },
-//        complete: function () {
-
-//            alert("4");
-//        }
-//    });
-//}
-
-//function UpdateProduct() {
-//    alert("jj");
-
-//    let Product = {
-//        ID: Id,
-//        TitleProduct: $('#txtTitleProduct').val(),
-//        Name: $('#txtOnvaneProduct').val(),
-//        EnName: $('#txtEnProduct').val(),
-//        CatProductId: 1,
-//        Coding: $('#txtCodeProduct').val(),
-//        Price: $('#txtPriceProduct').val(),
-//        FirstCount: $('#MojodiProduct').val(),
-//        ProductImages: '',
-//        ProductMeterId: parseInt($('#cmbVahed').val()),
-//        MatneProduct: tinyMCE.activeEditor.getContent()
-//    }
-
-//    alert("hj");
-//    jQuery.ajax({
-//        type: "Post",
-//        url: "api/Product/EditProduct",
-//        data: JSON.stringify(Product),
-//        async: false,
-//        contentType: "application/json; charset=utf-8",
-//        dataType: "json",
-//        success: function (response) {
-//            alert("i");
-//            Swal.fire(
-//                'ثبت شد !',
-//                'درباره ما با موفقیت بروز رسانی شد',
-//                'success'
-//            );
-
-//            GetAllAbout();
+            $('#txtTitleProduct').val("gbcv");
 
 
-//            $('#txtTitleProduct').val('');
-            
-//            $('#InsertModal').hide();
-//            $('#PnlList').show();
 
+            $('#InsertModal').show();
+            $('#PnlList').hide();
 
-//        },
-//        error: function (response) {
+        },
+        error: function (response) {
+            alert("13");
+            console.log(response);
 
-//            console.log(response);
-            
+        },
+        complete: function () {
 
-//        },
-//        complete: function () {
+            alert("4");
+        }
+    });
+}
 
-//        }
-//    });
-//}
+function UpdateProduct() {
+    alert("jj");
+
+    let Product = {
+        ID: Id,
+        TitleProduct: $('#txtTitleProduct').val(),
+        Name: $('#txtOnvaneProduct').val(),
+        EnName: $('#txtEnProduct').val(),
+        CatProductId: 1,
+        Coding: $('#txtCodeProduct').val(),
+        Price: $('#txtPriceProduct').val(),
+        FirstCount: $('#MojodiProduct').val(),
+        ProductImages: '',
+        ProductMeterId: parseInt($('#cmbVahed').val()),
+        MatneProduct: tinyMCE.activeEditor.getContent()
+    }
+
+    alert("hj");
+    jQuery.ajax({
+        type: "put",
+        url: "api/Product/EditProduct",
+        data: JSON.stringify(Product),
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            alert("i");
+            Swal.fire(
+                'ثبت شد !',
+                'درباره ما با موفقیت بروز رسانی شد',
+                'success'
+            );
+            GetAllAbout();
+            $('#txtTitleProduct').val('');            
+            $('#InsertModal').hide();
+            $('#PnlList').show();
+        },
+        error: function (response) {
+            console.log(response);           
+
+        },
+        complete: function () {
+
+        }
+    });
+}
 
 
 
@@ -253,30 +241,30 @@ $(document).ready(() => {
         //    });
         //} else {
         
-            //if (Id === 0) {
-            //    AddProduct();
-            //} else {
-            //    alert("1");
-            //    UpdateProduct();
-            //}
+            if (Id === 0) {
+                AddProduct();
+            } else {
+                alert("1");
+                UpdateProduct();
+            }
 
         //}
 
 
     });
-    //$(document.body).on('click', '.Edit', function () {
+    $(document.body).on('click', '.Edit', function () {
 
-    //    Id = parseInt($(this).attr('ProductID'));
-    //    alert("1");
-    //    GetProductById();
+        Id = parseInt($(this).attr('ProductID'));
+        alert("1");
+        GetProductById();
 
-    //});
+    });
 
-    //$(document.body).on('click', '#btnEnseraf', function () {
+    $(document.body).on('click', '#btnEnseraf', function () {
 
-    //    $('#InsertModal').hide();
-    //    $('#PnlList').show();
-    //});
+        $('#InsertModal').hide();
+        $('#PnlList').show();
+    });
 
     //$('#txtAzTarikh').persianDatepicker({
     //    observer: true,
