be7bf8507de596a90c32c161cb76559443da1199
azari <azari@DESKTOP-07J35GD>
2020-09-11 19:09:32 +0430

    990621-19

--- OnlineShop/wwwroot/Admin/FormScripts/Productui.js
+++ OnlineShop/wwwroot/Admin/FormScripts/Productui.js
@@ -1,19 +1,19 @@
-﻿
-let Id = 0;
-function Product() {
+﻿//import { id } from "module";
 
-    let Id = parseInt(window.location.pathname.replace('/Home/Product/', ''));
+let Id = 0;
+
+function GetProductVige() {
     let Html = ``;
 
     jQuery.ajax({
         type: "Get",
-        url: `/api/Product/GetProductInfoById?producId=${Id}`,
+        url: "/api/Product/GetTopProductListWithRate",
         data: "",
         async: false,
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (response) {
-            console.log(response);
+
 
             jQuery.each(response, function (i, item) {
                 Html += `<div class="product-thumb clearfix vighe">
@@ -48,10 +48,63 @@ function Product() {
     });
 }
 
+function Product() {
+
+    let Id = parseInt(window.location.pathname.replace('/Home/Product/', ''));
+    console.log(Id);
+    let Html = ``;
+
+    jQuery.ajax({
+        type: "Get",
+        url: `/api/Product/GetProductInfoById?producId=${Id}`,
+        data: "",
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+
+            jQuery.each(response, function (i, item) {
+             
+
+                Html += `<div class="product-thumb clearfix vighe">
+                            <div class="image">
+                                <a href="Home/Product/` + item.id + `">
+                                       <img src="` + item.coverImageUrl + `" alt="` + item.name + `" title="` + item.name + `" class="img-responsive" />
+                                </a>
+                            </div>
+                            <div class="caption">
+                                 <h4><a href="Home\Product?` + item.id + `">` + item.name + `</a></h4>
+                                 <p class="price">
+                                    <span class="price-new">` + item.price + `</span> 
+                                    <span class="price-old">` + item.price + `</span> 
+                                    <span class="saving">0</span> 
+                                 </p>
+                            </div>
+                        </div>`;
+
+                 
+            });
+
+            $('.product').html(Html);
+
+
+        },
+        error: function (response) {
+
+            console.log(response);
+        },
+        complete: function () {
+        }
+
+
+    });
+}
+
 
 
 $(document).ready(() => {
-
+    
     GetProductVige();
     Product();
 });
