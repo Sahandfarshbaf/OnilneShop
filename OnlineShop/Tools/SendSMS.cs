080985a6e08ea0d783312dfcba5869505ce57614
Sahand Farshbaf Golnary <mamalilaminet@gmail.com>
2020-09-16 00:52:38 +0430

    990626-00:55

---
+++ OnlineShop/Tools/SendSMS.cs
@@ -0,0 +1,43 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Tools
+{
+    public class SendSMS
+    {
+        public bool SendRegisterSMS(string mobileNo, string username, string pass)
+        {
+
+            var smsText = "نام کاربری و کلمه عبور شما در بازارچه اینترنتی صنایع دستی به شرح زیر می باشد:";
+            smsText += "\\n";
+            smsText += "نام کاربری: ";
+            smsText += username;
+            smsText += "\\n";
+            smsText += "کلمه عبور: ";
+            smsText += pass;
+            smsText += "\\n";
+            smsText += "لطفا به منظور ورود به فروشگاه ، به آدرس زیر مراجعه نمایید:";
+            smsText += "\\n";
+            smsText += "tabrizhandicrafts.com";
+
+            var client = new RestClient("http://188.0.240.110/api/select");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("undefined", "{\"op\" : \"send\"" +
+                                              ",\"uname\" : \"09144198583\"" +
+                                              ",\"pass\":  \"1375989081\"" +
+                                              ",\"message\" :"+
+                                              $" \"{smsText}\"" +
+                                              ",\"from\": \" +98500010407308865\"" +
+                                              $",\"to\" : [\"{mobileNo}\"]}}"
+                    , ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+            return response.IsSuccessful;
+        }
+    }
+}
